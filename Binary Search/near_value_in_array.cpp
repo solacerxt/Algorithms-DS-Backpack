cfde6ddd876f828d2807f4110e48fb70f9997e03
solacerxt <59528820+solacerxt@users.noreply.github.com>
2022-12-15 14:01:06 +0300

    Create near_value_in_array.cpp

---
+++ Binary Search/near_value_in_array.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <cstdlib>
+
+int search(const int val, const std::vector<int>& a) {
+    int l = -1;
+    int r = (int)a.size();
+
+    while (r - l > 1) {
+        int m = (l + r) / 2;
+        if (a[m] > val)
+            r = m;
+        else l = m;
+    }
+    
+    if (l == -1)
+        return a[r];
+    if (r != a.size() && a[r] - val < val - a[l])
+        return a[r];
+    return a[l];
+}
