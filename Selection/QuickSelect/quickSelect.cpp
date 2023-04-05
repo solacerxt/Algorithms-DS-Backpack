fe8805eb1c6edf0bfd888bd4b4b2d651677221de
solacerxt <59528820+solacerxt@users.noreply.github.com>
2023-04-05 11:27:59 +0300

    Create quickSelect.cpp

---
+++ Selection/QuickSelect/quickSelect.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <chrono>
+#include <random>
+
+void partition(std::vector<int>& a, int x, int st, int end, int& l, int& r) {
+    l = st;
+    int i = l;
+    while (i < end) {
+        while (i >= l && a[i] < x) {
+            std::swap(a[i], a[l]);
+            ++l;
+        }
+        ++i;
+    }
+    i = r = l;
+    while (i < end) {
+        while (i >= r && a[i] == x) {
+            std::swap(a[i], a[r]);
+            ++r;
+        }
+        ++i;
+    }
+}
+
+int quickSelect(std::vector<int>& a, int k, int st = 0, int end = -1) {
+    static std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+
+    if (end == -1) {
+        end = (int)a.size();
+    }
+    if (end - st == 1) {
+        return a[st];
+    }
+    int x = a[rng() % (end - st) + st];
+    int l, r;
+    partition(a, x, st, end, l, r);
+    if (k <= l) {
+        return quickSelect(a, k, st, l);
+    }
+    if (k <= r) {
+        return x;
+    }
+    return quickSelect(a, k, r, end);
+}
