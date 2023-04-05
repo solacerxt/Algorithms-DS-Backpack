410838d1f40b9e2518230da93b23648ab3b18dd4
solacerxt <59528820+solacerxt@users.noreply.github.com>
2023-04-05 11:32:52 +0300

    Update quickSelect.cpp

--- Selection/QuickSelect/quickSelect.cpp
+++ Selection/QuickSelect/quickSelect.cpp
@@ -2,8 +2,8 @@
 #include <chrono>
 #include <random>
 
-void partition(std::vector<int>& a, int x, int st, int end, int& l, int& r) {
-    l = st;
+void partition(std::vector<int>& a, int x, int start, int end, int& l, int& r) {
+    l = start;
     for (int i = l; i < end; ++i) {
         while (i >= l && a[i] < x) {
             std::swap(a[i], a[l]);
@@ -20,20 +20,20 @@ void partition(std::vector<int>& a, int x, int st, int end, int& l, int& r) {
     }
 }
 
-int quickSelect(std::vector<int>& a, int k, int st = 0, int end = -1) {
+int quickSelect(std::vector<int>& a, int k, int start = 0, int end = -1) {
     static std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 
     if (end == -1) {
         end = (int)a.size();
     }
-    if (end - st == 1) {
-        return a[st];
+    if (end - start == 1) {
+        return a[start];
     }
-    int x = a[rng() % (end - st) + st];
+    int x = a[rng() % (end - start) + start];
     int l, r;
-    partition(a, x, st, end, l, r);
+    partition(a, x, start, end, l, r);
     if (k <= l) {
-        return quickSelect(a, k, st, l);
+        return quickSelect(a, k, start, l);
     }
     if (k <= r) {
         return x;
