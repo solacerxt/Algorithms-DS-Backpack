6cf84dd12d3b1395190912fc4b922f09500131e7
solacerxt <59528820+solacerxt@users.noreply.github.com>
2023-07-07 16:45:26 +0300

    Create BinaryIndexedTree.cs

---
+++ Range Query/Binary Indexed Tree/BinaryIndexedTree.cs
@@ -0,0 +1,35 @@
+public class BinaryIndexedTree
+{
+    private readonly int _length;
+    private readonly int[] _array;
+
+    public BinaryIndexedTree(int length)
+    {
+        _array = new int[length];
+        _length = length;
+    }
+
+    public int Sum(int r)
+    {
+        var i = r - 1;
+        var ret = 0;
+
+        while (i >= 0)
+        {
+            ret += _array[i];
+            i = (i & (i + 1)) - 1;
+        }
+
+        return ret;
+    }
+
+    public void Increase(int i, int value)
+    {
+        var j = i;
+        while (j < _length)
+        {
+            _array[j] += value;
+            j |= (j + 1);
+        }
+    }
+}
